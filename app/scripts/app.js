3d223d4fd484f3a10dcead2343c6461620d04d94
Joe Martella <martellaj@live.com>
2015-11-11 09:17:04 -0800

    Fixed some whitespace.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,7 @@
 /*
 * Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license at the bottom of this file.
 */
+
 (function () {
   angular
     .module('profileApp', [
